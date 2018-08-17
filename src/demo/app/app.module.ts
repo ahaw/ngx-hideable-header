f5352bb5a95ab675bec241ca99ba55a8eb48fff7
ahaw <ahaw@o2.pl>
2018-08-17 11:38:46 +0200

    Changed version and make use of published npm package instead of local one.

--- src/demo/app/app.module.ts
+++ src/demo/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 // tslint:disable-next-line:no-implicit-dependencies
 // import { HideableHeaderModule } from "@ngx-hideable-header";
-import { HideableHeaderModule } from "@ngx-hideable-header-builded";
-// import { HideableHeaderModule } from 'ngx-hideable-header';
+// import { HideableHeaderModule } from "@ngx-hideable-header-builded";
+import { HideableHeaderModule } from 'ngx-hideable-header';
 
 
 import { HeaderComponent } from "./header/header.component";
