c049b43a0495e5d3d48a9b57490940bd956a1a18
Tane Piper <piper.tane@gmail.com>
2018-08-20 16:00:21 +0200

    Improve demo with use of subjects

--- src/demo/app/header/header.component.ts
+++ src/demo/app/header/header.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
 import { HideableHeaderDirective } from '../../../lib/public_api';
+import { Subject, BehaviorSubject, combineLatest } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'ngx-root',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-  disable: boolean = false;
-  reverse: boolean = false;
+export class HeaderComponent implements AfterViewInit, OnDestroy {
+  disable = new BehaviorSubject<boolean>(false);
+  reverse = new BehaviorSubject<boolean>(false);
 
-  @ViewChild(HideableHeaderDirective) hidableElement: HideableHeaderDirective;
+  private gc$ = new Subject<boolean>();
+
+  @ViewChild(HideableHeaderDirective)
+  hidableElement: HideableHeaderDirective;
 
   hide() {
     this.hidableElement.hide();
@@ -22,5 +27,20 @@ export class HeaderComponent {
 
   switchTransition(transition: string) {
     this.hidableElement.transition = transition;
+  }
+
+  ngAfterViewInit() {
+    combineLatest(this.hidableElement.isHidden, this.disable)
+      .pipe(takeUntil(this.gc$))
+      .subscribe(([isHidden, isDisabled]) => {
+        if (isHidden && isDisabled) {
+          this.hidableElement.show();
+        }
+      });
+  }
+
+  ngOnDestroy() {
+    this.gc$.next(true);
+    this.gc$.complete();
   }
 }
